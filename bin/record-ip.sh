8e49df8d6fbab86e8477c5fb18acecceb1c17c2c
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-05-29 00:06:38 +0900

    TAB文字を削除

--- bin/record-ip.sh
+++ bin/record-ip.sh
@@ -3,7 +3,7 @@ basedir="`dirname "$0"`/.."
 postmap="/usr/sbin/postmap"
 
 if [ ! -z "$IP" ]; then
-	ip=${IP#::ffff:}
+  ip=${IP#::ffff:}
   echo -e "$ip OK `date +%s`" | "$postmap" -ir "$basedir/db/allow_clients"
 fi
 exec "$@"
